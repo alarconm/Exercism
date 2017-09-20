d01c0f417d92cedd3b7e545302645cbc57fe635b
Michael Alarcon <malarconii@yahoo.com>
2017-09-19 23:02:44 -0700

    Completed third problem set

---
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,25 @@
+class RnaTranscription {
+
+    String transcribe(String dnaStrand) {
+
+        String rnaStrand = "";
+
+        for (int i = 0; i < dnaStrand.length(); i++) {
+
+            if (dnaStrand.charAt(i) == 'G') {
+                rnaStrand += 'C';
+            } else if (dnaStrand.charAt(i) == 'C') {
+                rnaStrand += 'G';
+            } else if (dnaStrand.charAt(i) == 'T') {
+                rnaStrand += 'A';
+            } else if (dnaStrand.charAt(i) == 'A') {
+                rnaStrand += 'U';
+            } else {
+                throw new IllegalArgumentException("Invalid input");
+            }
+
+        }
+
+        return rnaStrand;
+    }
+}
