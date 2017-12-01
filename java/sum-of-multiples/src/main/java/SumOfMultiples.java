0cbaf34cdfceb9086ee08b0fb49bff1adff9dd12
Mike <malarconii@yahoo.com>
2017-11-30 21:34:48 -0800

    completed problem set all tests passing

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -4,12 +4,16 @@ class SumOfMultiples {
 
     SumOfMultiples(int number, int[] set) {
 
-        for(int i = 1; i <= number; i++) {
-            if(i % set[0] == 0 || i % set[1] == 0) {
-                this.sum += i;
+        for(int i = 1; i < number; i++) {
+
+            for(int k = 0; k < set.length; k++) {
+
+                if (i % set[k] == 0) {
+                    this.sum += i;
+                    break; //if it is a multiple of ANY of the set add it just once, then move to the next number
+                }
             }
         }
-
     }
 
     int getSum() {
