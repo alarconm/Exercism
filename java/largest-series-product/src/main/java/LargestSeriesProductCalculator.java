1a799488c4a12cd6a30f799ea62c77e377c05f04
Mike <malarconii@yahoo.com>
2017-12-16 18:55:36 -0800

    problem set completed, all tests passing

--- java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
+++ java/largest-series-product/src/main/java/LargestSeriesProductCalculator.java
@@ -1,14 +1,23 @@
 class LargestSeriesProductCalculator {
 
-    private int[] input;
+    private long[] input;
     private int inputLength;
 
     LargestSeriesProductCalculator(String inputNumber) {
+
+        if (inputNumber == null) {
+            throw new IllegalArgumentException("String to search must be non-null.");
+        }
+
         this.inputLength = inputNumber.length();
-        int[] inputArray = new int[inputLength];
+        long[] inputArray = new long[inputLength];
 
         for (int i = 0; i < inputLength; i++) {
             inputArray[i] = Character.getNumericValue(inputNumber.charAt(i));
+
+            if (inputArray[i] > 9) {
+                throw new IllegalArgumentException("String to search may only contain digits.");
+            }
         }
 
         this.input = inputArray;
@@ -16,13 +25,21 @@ class LargestSeriesProductCalculator {
 
     long calculateLargestProductForSeriesLength(int numberOfDigits) {
 
+        if (numberOfDigits > inputLength) {
+            throw new IllegalArgumentException(
+                    "Series length must be less than or equal to the length of the string to search.");
+        }
+        if (numberOfDigits < 0 ) {
+            throw new IllegalArgumentException("Series length must be non-negative.");
+        }
+
         long highestProduct = 0;
         int length = inputLength - numberOfDigits;
 
-        for (int i = 0; i < length; i++) {
-            int temp = 1;
+        for (int i = 0; i <= length; i++) {
+            long temp = 1;
 
-            for(int j = 0; j < numberOfDigits; j++) {
+            for (int j = 0; j < numberOfDigits; j++) {
                 temp *= input[j + i];
             }
 
