6b7031c7c4a6182720c12ebc34581c9110b87a42
Mike <malarconii@yahoo.com>
2018-01-05 15:55:58 -0800

    Problem set completed with all tests passing

--- java/kindergarten-garden/src/main/java/KindergartenGarden.java
+++ java/kindergarten-garden/src/main/java/KindergartenGarden.java
@@ -1,19 +1,31 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class KindergartenGarden {
 
-    List<String> studentList;
-    List<Plant[]> plants = new ArrayList<>();
+    private List<String> studentList;
+    private HashMap<String, List<Plant>> studentPlants = new HashMap<>();
 
     KindergartenGarden(String garden, String[] students) {
+        Arrays.sort(students);
+        String[] rows = garden.split("\n");
+        Plant[][] plants =new Plant[(garden.length() -1) / 2][4];
+        this.studentList = Arrays.asList(students);
 
-
+        int ph = 0; //placeholder to count index by 1 while looping by 2
+        for (int i = 0; i < rows[0].length(); i += 2) {
+            plants[ph][0] = Plant.getPlant(rows[0].charAt(i));
+            plants[ph][1] = Plant.getPlant(rows[0].charAt(i + 1));
+            plants[ph][2] = Plant.getPlant(rows[1].charAt(i));
+            plants[ph][3] = Plant.getPlant(rows[1].charAt(i + 1));
+            studentPlants.put(studentList.get(ph), Arrays.asList(plants[ph]));
+            ph++;
+        }
     }
 
     KindergartenGarden(String garden) {
 
+        String[] rows = garden.split("\n");
+        Plant[][] plants =new Plant[(garden.length() -1) / 2][4];
         String[] studentArray = {
                 "Alice",
                 "Bob",
@@ -30,23 +42,18 @@ class KindergartenGarden {
         };
         this.studentList = Arrays.asList(studentArray);
 
-        int ph = 0;
-        for (int i = 0; i < garden.length(); i += 2) {
-
-            if (garden.charAt(i) == '\n') {
-                ph = 0;
-                i -= 1;
-            }
-            else {
-                plants[ph].add());
-                plants.add(ph, Plant.getPlant(garden.charAt(i+1)));
-                ph++;
-            }
+        int ph = 0; //placeholder to count index by 1 while looping by 2
+        for (int i = 0; i < rows[0].length(); i += 2) {
+            plants[ph][0] = Plant.getPlant(rows[0].charAt(i));
+            plants[ph][1] = Plant.getPlant(rows[0].charAt(i + 1));
+            plants[ph][2] = Plant.getPlant(rows[1].charAt(i));
+            plants[ph][3] = Plant.getPlant(rows[1].charAt(i + 1));
+            studentPlants.put(studentList.get(ph), Arrays.asList(plants[ph]));
+            ph++;
         }
-        System.out.println(plants);
     }
 
     List<Plant> getPlantsOfStudent(String student) {
-        return Arrays.asList(plants.get(studentList.indexOf(student)));
+        return studentPlants.get(student);
     }
 }
